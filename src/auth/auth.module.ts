4046691feb11b6effc71fe861a241400b2b74fe9
Amanbanti <amanbanti2011@gmail.com>
2025-02-14 21:45:34 +0300

    createing auth/user module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { User } from './entities/auth.entity';
 
 @Module({
-  controllers: [AuthController],
+  imports: [SequelizeModule.forFeature([User])],
   providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
+
