cdfaee63d907166de4c34be1030b178779431cd2
Amanbanti <amanbanti2011@gmail.com>
2025-02-14 21:41:45 +0300

    sequelize initiallization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { User } from './auth/entities/auth.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest',
+      autoLoadModels: true, // Automatically loads models
+      synchronize: true, //Sync DB schema (set to false in production)
+      models: [User], //Explicitly add User model
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
